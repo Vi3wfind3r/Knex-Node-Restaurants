f2a897ddbe805363c2aff6d5e574ca8bb3605d86
Jamie Wang <sephona@gmail.com>
2017-05-31 11:22:31 -0400

    coded drills one through five

--- server.js
+++ server.js
@@ -11,15 +11,50 @@ const app = express();
 app.use(bodyParser.json());
 
 //Get all restaurants//
+// app.get('/restaurants', (req, res) => {
+
+//   knex.select()
+//     .from('restaurants')
+//     .then(results => res.json(results));
+// });
+
+//Get Italian Restaurants//
+// app.get('/restaurants', (req, res) => {
+
+//   knex.select()
+//     .from('restaurants')
+//     .where('cuisine', 'Italian')
+//     .then(results => res.json(results));
+// });
+
+//Get 10 Italian restaurants, subset of fields//
+// app.get('/restaurants', (req, res) => {
+
+//   knex.select('id', 'name')
+//     .from('restaurants')
+//     .where('cuisine', 'Italian')
+//     .limit(10)
+//     .then(results => res.json(results));
+// });
+
+//Count of Thai restaurants//
+// app.get('/restaurants', (req, res) => {
+
+//   knex.select()
+//     .from('restaurants')
+//     .where('cuisine', 'Thai')
+//     .count()
+//     .then(results => res.json(results));
+// });
+
+//Count of restaurants//
 app.get('/restaurants', (req, res) => {
 
   knex.select()
     .from('restaurants')
+    .count()
     .then(results => res.json(results));
 });
-
-//Get Italian Restaurants//
-
 
 app.listen(PORT);
 
